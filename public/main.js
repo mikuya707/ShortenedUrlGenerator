38c69ba13ee14c2fbfdd39f6612768c512865182
Karen Mao <karen.mao707@gmail.com>
2016-05-01 10:20:21 -0400

    changed route logic.

--- public/main.js
+++ public/main.js
@@ -142,10 +142,10 @@ module.exports = TableComponent;
 },{"griddle-react":19,"react":176}],3:[function(require,module,exports){
 var urls = [{
     "original url": "http://facebook.com",
-    "shortened url": "http://shorten-url-generator.herokuapp.com/78oz4"
+    "shortened url": "shorten-url-generator.herokuapp.com/78oz4"
 },{
     "original url": "http://google.com",
-    "shortened url": "http://shorten-url-generator.herokuapp.com/0GUcW"
+    "shortened url": "shorten-url-generator.herokuapp.com/0GUcW"
 }];
 
 module.exports = urls;
