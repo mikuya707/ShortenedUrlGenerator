fa339296ff7f121a25e18d721f097a1a8df4bd0a
Karen Mao <karen.mao707@gmail.com>
2016-05-01 10:53:41 -0400

    length of url

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -27,10 +27,13 @@ module.exports = function(app) {
         if(shortCode !== "favicon.ico"){
             console.log("what is urls", urls);
             urls.forEach(function(url){
-                if(url['shortened url'].slice(43) == shortCode){
+                if(url['shortened url'].slice(1) == shortCode){
                     var longUrl = url['original url']
                     console.log("redirection takes place", longUrl);
                     res.redirect(longUrl);
+                }
+                else{
+                    console.log('url format error.');
                 }
             })
 
